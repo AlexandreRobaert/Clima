2ad025ea21fcdd06592883bb0dc89e57b177fd3f
Alexandre Robaert <robaert.developer@icloud.com>
2021-04-05 15:05:27 -0300

    Projeto com layout pronto, actions e outlets OK

--- Clima/Controller/WeatherViewController.swift
+++ Clima/Controller/WeatherViewController.swift
@@ -13,12 +13,48 @@ class WeatherViewController: UIViewController {
     @IBOutlet weak var conditionImageView: UIImageView!
     @IBOutlet weak var temperatureLabel: UILabel!
     @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var searchTextField: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        searchTextField.delegate = self
     }
-
-
+    
+    @IBAction func searchPressed(_ sender: UIButton) {
+        searchTextField.endEditing(false)
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
+    }
 }
 
+extension WeatherViewController: UITextFieldDelegate {
+    
+   
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        
+        if textField.text!.count > 3 {
+            textField.endEditing(false)
+            return true
+        }
+        
+        textField.text = ""
+        textField.placeholder = "Você deve inserir uma cidade aqui"
+        return false
+    }
+    
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        if textField.text!.count > 3 {
+            return true
+        }
+        textField.placeholder = "Você deve inserir uma cidade aqui."
+        textField.text = ""
+        return false
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        textField.placeholder = "Search"
+        textField.text = ""
+    }
+}
